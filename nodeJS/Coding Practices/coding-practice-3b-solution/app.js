647109aac743833cabb695bc36fb8ad10fcab56e
Devesh-Bandi <devesh.bandi5@gmail.com>
2023-11-18 12:16:48 +0530

    del

--- nodeJS/Coding Practices/coding-practice-3b-solution/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/gadgets", (request, response) => {
-  response.sendFile("./gadgets.html", { root: __dirname });
-});
-
-module.exports = app;
