68bdcaac96e56db493481f48af8cacd9e9e2879a
Devesh-Bandi <devesh.bandi5@gmail.com>
2023-11-18 15:08:20 +0530

    ExpressJS

---
+++ nodeJS/ExpressJS/Date after 100 Days from Today/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const addDays = require("date-fns/addDays");
+const app = express();
+app.get("/", (request, response) => {
+  let dateTime = new Date();
+  let result = addDays(
+    new Date(dateTime.getFullYear(), dateTime.getMonth(), dateTime.getDate()),
+    100
+  );
+  response.send(
+    `${result.getDate()}/${result.getMonth() + 1}/${result.getFullYear()}`
+  );
+});
+module.exports = app;
